9413fce1def27a4e57abd4ba94b10ae5565e8c36
haris <93856015+HarisBalaji@users.noreply.github.com>
2022-09-25 11:21:06 +0530

    App_Using_ImageView

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.calculator
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private  var tvInput:TextView?=null
+    var lastNum : Boolean=false
+    var lastDot : Boolean=false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvInput=findViewById(R.id.tvInput)
+    }
+
+    fun onDigit(view : View) {
+        tvInput?.append((view as Button).text)
+        lastNum = true
+        lastDot = false
+    }
+
+    fun onClear(view:View){
+        tvInput?.text=""
+    }
+
+    fun onDecimal(view: View){
+        if(lastNum && !lastDot){
+            tvInput?.append(".")
+            lastNum=false
+            lastDot=true
+        }
+    }
+
+    fun onOperator(view:View){
+        tvInput?.text?.let{
+            if (lastNum && !isOperator(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNum=false
+                lastDot=false
+            }
+        }
+    }
+
+    private fun isOperator(value:String):Boolean{
+        return if (value.startsWith("-")) {
+            false
+        }else{
+            value.contains("/") || value.contains("x") || value.contains("+")
+                    || value.contains("-")
+        }
+    }
+
+    fun onEqual(view:View){
+        if (lastNum){
+            var tvValue=tvInput?.text.toString()
+            var prefix=""
+            try{
+                if(tvValue.startsWith("-")){
+                    prefix="-"
+                    tvValue=tvValue.substring(1)
+                }
+                if(tvValue.contains("-")){
+                    var splitValue=tvValue.split("-")
+
+                    var one=splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+                    tvInput?.text=removeDot((one.toDouble()-two.toDouble()).toString())
+                }
+                else if(tvValue.contains("+")){
+                    var splitValue=tvValue.split("+")
+
+                    var one=splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+                    tvInput?.text=removeDot((one.toDouble()+two.toDouble()).toString())
+                }
+                else if(tvValue.contains("x")){
+                    var splitValue=tvValue.split("x")
+
+                    var one=splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+                    tvInput?.text=removeDot((one.toDouble()*two.toDouble()).toString())
+                }
+                else if(tvValue.contains("/")){
+                    var splitValue=tvValue.split("/")
+
+                    var one=splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+                    tvInput?.text=removeDot((one.toDouble()/two.toDouble()).toString())
+                }
+
+            }catch(e:ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun removeDot(result:String):String{
+        var value=result
+        if(result.contains(".0"))
+            value=result.substring(0,result.length-2)
+        return  value
+    }
+
+}
